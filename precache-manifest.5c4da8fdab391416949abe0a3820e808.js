87c37ac6af57d5c1944b455a023acf984846b784
Ciobanu Vlad <ciobanu.vlad@cerner.com>
2019-07-23 21:57:43 +0300

    Updates

---
+++ precache-manifest.5c4da8fdab391416949abe0a3820e808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2db945301d1a1d7a6074fc07bdc579d",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "455c7a4d21c26824934d",
+    "url": "/travel/static/css/main.45d48af5.chunk.css"
+  },
+  {
+    "revision": "1cb7f25da24c1ab7c136",
+    "url": "/travel/static/js/2.048e779e.chunk.js"
+  },
+  {
+    "revision": "455c7a4d21c26824934d",
+    "url": "/travel/static/js/main.fbaa7516.chunk.js"
+  },
+  {
+    "revision": "863f8cee14e2ca72744b",
+    "url": "/travel/static/js/runtime~main.11399f7e.js"
+  },
+  {
+    "revision": "06b6bb10364e125354d34b05f9c28eac",
+    "url": "/travel/static/media/iconTrip.06b6bb10.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travel/static/media/logo.5d5d9eef.svg"
+  }
+]);
